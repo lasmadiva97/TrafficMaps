24a6a247f510bb78ca4fb61335d7167ea843fcc4
Lasmadiva97 <lasmadiva97@gmail.com>
2019-05-18 09:55:31 +0700

    isi

---
+++ app/src/main/java/com/example/trafficmaps/LocationFragment.java
@@ -0,0 +1,26 @@
+package com.example.trafficmaps;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LocationFragment extends Fragment{
+
+    public LocationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.fragment_location,container, false);
+
+        return v;
+    }
+
+}
